755dccb90722c4ea3c05051a626e13f3ca62b1cb
EC2 Default User <ec2-user@ip-172-31-45-13.ec2.internal>
2019-11-07 01:33:14 +0000

    Nov 6 - Updated changes

--- jupyter/utilities/pythonDB.py
+++ jupyter/utilities/pythonDB.py
@@ -1,4 +1,5 @@
 import mysql.connector
+
 def writeToDB(optimizer, activation, architecture, bag, label=None, keywords=None):
     loss, accuracy, mae, mse, train_loss_dict, train_accuracy_dict, train_mae_dict, train_mse_dict = bag
     db_conn = mysql.connector.connect(host="localhost", user="root", passwd="1234",  database='cs682research')
@@ -11,4 +12,16 @@ def writeToDB(optimizer, activation, architecture, bag, label=None, keywords=Non
     db_cursor.close()
     db_conn.close()
     print ("Successfully written to the database...")
+    return
+
+def deleteExistingPrimaryKeyDB(optimizer, activation, architecture, label=None, keywords=None):
+    db_conn = mysql.connector.connect(host="localhost", user="root", passwd="1234",  database='cs682research')
+    db_cursor = db_conn.cursor(buffered=True)
+    delete_query = "DELETE FROM research_exploration WHERE optimizer='{}' and activation='{}' and architecture='{}' \
+and label='{}' and keywords='{}'".format(optimizer, activation, architecture, label, keywords)
+    db_cursor.execute(delete_query)
+    db_conn.commit()
+    db_cursor.close()
+    db_conn.close()
+    print ("Successfully deleted the existing primary key...")
     return
